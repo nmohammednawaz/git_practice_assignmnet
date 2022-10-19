91ce57b2af8c533b3b33ee1cb527ad5413458f2c
nmohammednawaz <nmohammednawaz786@gmail.com>
2022-10-19 12:29:01 +0530

    Added_Prime_And_Palindrome_Program

---
+++ palindrome.js
@@ -0,0 +1,16 @@
+// Program to check whether a string is palindrome or not.
+
+let string = "ababab";
+let isPalindrome = false;
+
+// Ieterate loop from 0 to length of the string
+for(let i = 0; i < string.length; i++){
+  if(string[i] == string[string.length-i]){
+    isPalindrome = true;
+  }
+}
+if(isPalindrome){
+  console.log(string,"is palindrome");
+}else{
+  console.log(string,"is not palindrome");
+}
